2d03c742aa1af7623db435d3aec1f3b8e640930b
Ewan Smith <ewanwsmith@gmail.com>
2023-11-03 14:21:22 +0000

    match_orfs() by length

--- src/functions.jl
+++ src/functions.jl
@@ -26,10 +26,37 @@ function convert_to_date(folder::String)
 end
 
 # read in fasta data
+function fasta_readin(filename::AbstractString)
+    fasta_dict = Dict{String, String}()
+    current_header = ""
+    current_sequence = ""
+
+    open(filename) do file
+        for line in eachline(file)
+            if startswith(line, ">")
+                if current_header != ""
+                    fasta_dict[current_header] = current_sequence
+                    current_sequence = ""
+                end
+                current_header = strip(line[2:end])
+            else
+                current_sequence *= strip(line)
+            end
+        end
+    end
+
+    if current_header != ""
+        fasta_dict[current_header] = current_sequence
+    end
+
+    return fasta_dict
+end
+
+# read in fasta data
 using DataFrames
 
 function find_orfs(fasta_dict::Dict{String, String})
-    orfs_df = DataFrame(Sequence_Name = String[], Start_Position = Int[], End_Position = Int[], ORF_Length = Int[], ORF_Sequence = String[])
+    orfs_df = DataFrame(Sequence_Name = String[], Start_Position = Int[], End_Position = Int[], ORF_Length = Int[], Sequence = String[])
 
     for (sequence_name, sequence) in fasta_dict
         orfs = find_orfs_in_sequence(sequence, sequence_name)
@@ -106,30 +133,82 @@ function translate_orfs(fasta_dict::Dict{String, Vector{String}})
     return translated_orfs
 end
 
+# read in reference FASTAs function
+function ref_readin(file_path::AbstractString)
+    orf_names = String[]
+    sequences = String[]
+    current_sequence = ""
+
+    open(file_path) do file
+        current_orf_name = ""
+        for line in eachline(file)
+            if occursin(">", line)
+                if current_orf_name != ""
+                    push!(orf_names, current_orf_name)
+                    push!(sequences, current_sequence)
+                    current_sequence = ""
+                end
+                current_orf_name = split(strip(line), '>')[2]
+            else
+                current_sequence *= strip(line)
+            end
+        end
+        if current_orf_name != ""
+            push!(orf_names, current_orf_name)
+            push!(sequences, current_sequence)
+        end
+    end
+
+    df = DataFrame(
+        Reference_orf_name = orf_names,
+        Sequence = sequences
+    )
+    df = hcat(df,
+    DataFrame(reduce(vcat, permutedims.(split.(df.Reference_orf_name, '|'))),
+    [:Gene_accession_and_position, :ORF_name]))
+    select!(df, Not(:Reference_orf_name))
+    df = hcat(df,
+    DataFrame(reduce(vcat, permutedims.(split.(df.Gene_accession_and_position, ':'))),
+    [:Accession, :Position]))
+    select!(df, Not(:Gene_accession_and_position))
+    df = hcat(df,
+    DataFrame(reduce(vcat, permutedims.(split.(df.Position, '-'))),
+    [:Start_Position, :End_Position]))
+    select!(df, Not(:Position))
+    select!(df, [:Accession, :ORF_name, :Start_Position, :End_Position, :Sequence])
+    df.Start_Position = parse.(Int64, df.Start_Position)
+    df.End_Position = parse.(Int64, df.End_Position)
+    return df
+end
+
+
 #first go at an orf-matching function
-using DataFrames
-using BioSequences
-using BioAlignments
+function match_orfs(orf_df::DataFrame, ref_orf_df::DataFrame)
+    result_df = DataFrame(ORF_name = String[], Sequence_Name = String[], Start_Position = Int[], End_Position = Int[], Matched_Sequence = String[])
 
-function match_orfs(orfs::Dict{String, Vector{String}}, reference_orfs::Dict{String, String})
-    result_df = DataFrame(Reference_orf_name = String[], Sample_name = String[], Sequence = String[])
+    for seq_name in unique(orf_df.Sequence_Name)
+        orf_df_subset = filter(row -> row.Sequence_Name == seq_name, orf_df)
+        for i in 1:size(ref_orf_df, 1)
+            ref_start = ref_orf_df[i, :Start_Position]
+            ref_end = ref_orf_df[i, :End_Position]
 
-    for (ref_name, ref_sequence) in reference_orfs
-        for (seq_name, orfs_list) in orfs
-            best_score = 0
-            best_match = ""
-            best_seq = ""
-            for orf in orfs_list
-                global_alignment = BioAlignments.pairalign(BioAlignments.LevenshteinDistance(), orf, ref_sequence)
-                score = BioAlignments.score(global_alignment)
-                if score >= best_score
-                    best_score = score
-                    best_match = seq_name
-                    best_seq = orf
+            best_match_idx = 0
+            min_distance = typemax(Int)
+            for j in 1:size(orf_df_subset, 1)
+                orf_start = orf_df_subset[j, :Start_Position]
+                orf_end = orf_df_subset[j, :End_Position]
+
+                if orf_start >= ref_start && orf_end <= ref_end
+                    distance = min(abs(orf_start - ref_start), abs(orf_end - ref_end))
+                    if distance < min_distance
+                        min_distance = distance
+                        best_match_idx = j
+                    end
                 end
             end
-            if best_score >= 0.8 # Adjust thresholds as needed
-                push!(result_df, (ref_name, best_match, best_seq))
+
+            if best_match_idx != 0
+                push!(result_df, (ref_orf_df[i, :ORF_name], seq_name, orf_df_subset[best_match_idx, :Start_Position], orf_df_subset[best_match_idx, :End_Position], orf_df_subset[best_match_idx, :Sequence]))
             end
         end
     end
@@ -141,25 +220,6 @@ end
 #test
 kemp_fasta = fasta_readin("data/Kemp/Sequences.fa")
 kemp_orfs = find_orfs(kemp_fasta)
-ref_orfs = fasta_readin("data/reference/coding_sequences.fasta")
-#kemp_matched_orfs = match_orfs(kemp_orfs, ref_orfs)
+ref_orfs = ref_readin("data/reference/coding_sequences.fasta")
+kemp_matched_orfs = match_orfs(kemp_orfs, ref_orfs)
 
-# chop and change column names to be more manageable
-function split_orfs_df(df::DataFrame)
-    df = hcat(df,
-    DataFrame(reduce(vcat, permutedims.(split.(df.Reference_orf_name, '|'))),
-    [:Gene_accession_and_position, :ORF_name]))
-    select!(df, Not(:Reference_orf_name))
-    df = hcat(df,
-    DataFrame(reduce(vcat, permutedims.(split.(df.Gene_accession_and_position, ':'))),
-    [:Accession, :Position]))
-    select!(df, Not(:Gene_accession_and_position))
-    df = hcat(df,
-    DataFrame(reduce(vcat, permutedims.(split.(df.Position, '-'))),
-    [:Start, :End]))
-    select!(df, Not(:Position))
-    select!(df, [:Accession, :Sample_name, :ORF_name, :Start, :End, :Sequence])
-    return df
-end
-
-#kemp_clean_orfs = split_orfs_df(kemp_matched_orfs)
