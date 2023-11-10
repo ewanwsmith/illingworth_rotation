92b324882e03e0ea67177dca4a2eb663d2fc07da
Ewan Smith <ewanwsmith@gmail.com>
2023-11-10 14:38:24 +0000

    add metadata join to ORF finding script

--- src/fasta_to_matched_ORFs.jl
+++ src/fasta_to_matched_ORFs.jl
@@ -166,6 +166,28 @@ end
 ref_orfs = ref_readin(reference_path)
 println("Reading in reference open reading frames from: $reference_path")
 
+function join_metadata(folder::String)
+    # Check if the folder contains the required files
+    matched_orfs_path = joinpath(folder, "matched_orfs.csv")
+    metadata_path = joinpath(folder, "metadata.csv")
+
+    if !isfile(matched_orfs_path) || !isfile(metadata_path)
+        println("Warning: One or both files not found in the folder.")
+        return
+    end
+
+    # Read the data into dataframes
+    matched_orfs_df = CSV.read(matched_orfs_path, DataFrame)
+    metadata_df = CSV.read(metadata_path, DataFrame)
+
+    # Perform the join operation
+    joined_df = rightjoin(matched_orfs_df, metadata_df, on = :Sequence_Name)
+
+    # Save the joined dataframe in the folder as matched_orfs.csv
+    joined_df_path = joinpath(folder, "matched_orfs.csv")
+    CSV.write(joined_df_path, joined_df)
+end
+
 # run above functions
 function find_match_orfs(folder::String)
     fasta_file = joinpath(folder, "sequences.fa") #find sample .fasta files in folder
@@ -181,7 +203,10 @@ function find_match_orfs(folder::String)
     println("Writing matched_orfs.csv to folder: $folder")
     CSV.write(csv_path, match_orfs_result) #write results to a CSV in sample folder
 
-    return match_orfs_result
+    joined_orf_and_metadata_result = join_metadata(folder)
+    println("ORF dataframe and Metadata dataframe have been joined and saved as matched_orfs.csv in $folder.")
+
+    return joined_orf_and_metadata_result
 end
 
 # run on folder_list folders
