15d2ed39edc671f72b2a736cfdde6adc6f048dcc
Ewan Smith <ewanwsmith@gmail.com>
2023-11-14 17:02:01 +0000

    variant_readin() can now handle blank .out files

--- src/output_variants.jl
+++ src/output_variants.jl
@@ -7,6 +7,16 @@ folder_list = ["data/CAMP000427", "data/CAMP001339", "data/CAMP001490",
 using DataFrames
 using CSV
 using FileIO
+using DelimitedFiles
+
+
+#read .out files to .csv
+using DataFrames
+using CSV
+using FileIO
+using DataFrames
+using CSV
+using FileIO
 
 function variant_readin(folder::AbstractString)
     # Construct the file path for Variant_list.out
@@ -14,23 +24,41 @@ function variant_readin(folder::AbstractString)
 
     # Check if the file exists
     if isfile(variant_file_path)
-        # Read the data from Variant_list.out with space delimiter
-        data = readdlm(variant_file_path, ' ', header=false)
+        # Attempt to read the data from Variant_list.out with space delimiter
+        try
+            data = readdlm(variant_file_path, ' ', header=false)
 
-        # Create a DataFrame
-        df = DataFrame(Position = data[:, 1], Original_Base = data[:, 2], Variant_Base = data[:, 3])
+            # Check if the file is empty or has no rows
+            if isempty(data) || all(isempty, eachrow(data))
+                # If empty, create an empty DataFrame with headers
+                df = DataFrame(Position = String[], Original_Base = String[], Variant_Base = String[])
+            else
+                # Create a DataFrame
+                df = DataFrame(Position = data[:, 1], Original_Base = data[:, 2], Variant_Base = data[:, 3])
+            end
 
-        # Save the DataFrame as a CSV file
-        csv_file_path = joinpath(folder, "Variant_list.csv")
-        CSV.write(csv_file_path, df)
+            # Save the DataFrame as a CSV file
+            csv_file_path = joinpath(folder, "Variant_list.csv")
+            CSV.write(csv_file_path, df)
 
-        println("DataFrame saved as CSV file: $csv_file_path")
+            println("DataFrame saved as CSV file: $csv_file_path")
+
+        catch
+            # If reading the file fails, create an empty DataFrame with headers
+            df = DataFrame(Position = String[], Original_Base = String[], Variant_Base = String[])
+
+            # Save the empty DataFrame as a CSV file
+            csv_file_path = joinpath(folder, "Variant_list.csv")
+            CSV.write(csv_file_path, df, writeheader=true)
+
+            println("Error: Unable to read Variant_list.out. Empty CSV file with headers created: $csv_file_path")
+        end
     else
         println("Error: Variant_list.out not found in the specified folder.")
     end
 end
 
-# run on folder_list folders
+
 for folder in folder_list
     variant_readin(folder)
 end
