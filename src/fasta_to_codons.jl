a4d6fabfaaf0b0a7b65ab8c35448fc438d172776
Ewan Smith <ewanwsmith@gmail.com>
2023-11-21 19:00:38 +0000

    add locate_variants()

--- src/fasta_to_codons.jl
+++ src/fasta_to_codons.jl
@@ -127,7 +127,7 @@ end
 
 # match found ORFs to reference ORFs by length and fewest mismatches
 function count_mismatches(seq1::AbstractString, seq2::AbstractString) #small function to count mismatches
-    return sum(seq1[i] != seq2[i] for i in 1:length(seq1))
+    return sum(seq1[i] != seq2[i] for i in eachindex(seq1))
 end
 
 function match_consensus_orfs(result_df::DataFrame)
@@ -162,6 +162,62 @@ function find_match_consensus_orfs(folder::String)
     return match_orfs_result
 end
 
-for folder in folder_list
-    find_match_consensus_orfs(folder)
+#for folder in folder_list
+#    find_match_consensus_orfs(folder)
+#end
+
+# read variants, find the ORFs they sit in
+function locate_variants(folder_path::String)
+    # Read Variant_list.csv into variants_df
+    variants_path = joinpath(folder_path, "Variant_list.csv")
+    variants_df = CSV.read(variants_path, DataFrame)
+
+    # Read Consensus_ORFs.csv into orfs_df
+    orfs_path = joinpath(folder_path, "Consensus_ORFs.csv")
+    orfs_df = CSV.read(orfs_path, DataFrame)
+
+    # Initialize variant_locations_df
+    variant_locations_df = DataFrame(
+        ORF_name = String[],
+        Start_Position = Int[],
+        End_Position = Int[],
+        Sequence = String[],
+        Variant_Position = Int[],
+        Original_Base = String[],
+        Variant_Base = String[]
+    )
+
+    # Count of no matches
+    no_match_count = 0
+
+    # Iterate through each row in variants_df
+    for i in 1:size(variants_df, 1)
+        position_value = variants_df[i, :Position]
+
+        # Find the row in orfs_df where Position is between Start_Position and End_Position
+        matching_row = filter(row -> row.Start_Position <= position_value <= row.End_Position, orfs_df)
+
+        # If a match is found, add a row to variant_locations_df
+        if !isempty(matching_row)
+            push!(variant_locations_df, (
+                matching_row[1, :ORF_name],
+                matching_row[1, :Start_Position],
+                matching_row[1, :End_Position],
+                matching_row[1, :Matched_Sequence],  # Assuming this column is present in orfs_df
+                variants_df[i, :Position],
+                variants_df[i, :Original_Base],
+                variants_df[i, :Variant_Base]
+            ))
+        else
+            # If no match is found, increment no_match_count
+            no_match_count += 1
+        end
+    end
+
+    # Print the count of rows with no matches
+    println("Number of variants with no matches: $no_match_count")
+
+    return variant_locations_df
 end
+
+kemp_located = locate_variants("data/Kemp")
