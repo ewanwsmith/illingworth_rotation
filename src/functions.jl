96e145d6e52f20e91ad2d86c26d911777fb4acaf
Ewan Smith <ewanwsmith@gmail.com>
2023-12-27 15:09:26 +0000

    add whitespace delimiter

--- src/functions.jl
+++ src/functions.jl
@@ -441,29 +441,36 @@ function find_rates(folder_path::AbstractString)
     rates_path = joinpath(folder_path, "Mean_rates.dat")
     probs_path = joinpath(folder_path, "Fixation_probabilities.dat") 
 
-    delimiters = ['\t', ' ']
+    try
+        # Try reading the files with tab delimiter
+        rates_df = CSV.File(rates_path, delim='\t', header=["Position", "Original_Base", "Variant_Base", "Evo_rate"]) |> DataFrame
+        probs_df = CSV.File(probs_path, delim='\t', header=["Position", "Original_Base", "Variant_Base", "Pr_fixation"]) |> DataFrame
 
-    for delimiter in delimiters
+        # Apply true_to_T() to the dataframes
+        rates_df = true_to_T(rates_df)
+        probs_df = true_to_T(probs_df)
+
+        return rates_df, probs_df
+    catch e_tab
         try
-            # Read the text files into DataFrames with the specified delimiter
-            rates_df = CSV.File(rates_path, delim=delimiter, header=["Position", "Original_Base", "Variant_Base", "Evo_rate"]) |> DataFrame
-            probs_df = CSV.File(probs_path, delim=delimiter, header=["Position", "Original_Base", "Variant_Base", "Pr_fixation"]) |> DataFrame
+            # If tab delimiter fails, try reading with whitespace delimiter
+            rates_df = readdlm(rates_path, Any, '\t') |> DataFrame
+            probs_df = readdlm(probs_path, Any, '\t') |> DataFrame
 
             # Apply true_to_T() to the dataframes
             rates_df = true_to_T(rates_df)
             probs_df = true_to_T(probs_df)
 
             return rates_df, probs_df
-        catch e
-            # Print an error message if the current delimiter failed
-            println("Error reading with delimiter $delimiter: $e")
+        catch e_space
+            # If both attempts fail, print error messages and return empty DataFrames
+            println("Error reading with tab delimiter: $e_tab")
+            println("Error reading with space delimiter: $e_space")
+            return DataFrame(), DataFrame()
         end
     end
-
-    # If no successful read, return empty DataFrames
-    println("Error reading the files. Could not determine the delimiter.")
-    return DataFrame(), DataFrame()
 end
+
 
 
 function join_rates(rates_df::DataFrame, probs_df::DataFrame)
